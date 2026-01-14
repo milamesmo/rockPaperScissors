f1952aae4ba63fa16472a6acf293f4707f1f3b34
milamesmo <camilaabegg.n@gmail.com>
2026-01-13 22:04:29 -0300

    add new autoplay btn and stylesheet

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
 let score = {
-    Wins: 0,
-    Losses: 0,
-    Ties: 0
+  Wins: 0,
+  Losses: 0,
+  Ties: 0,
+};
+
+function upgradeScore(result) {
+  if (result === "Tie") {
+    score.Ties += 1;
+  } else if (result === "Win") {
+    score.Wins += 1;
+  } else {
+    score.Losses += 1;
+  }
 }
 
-function upgradeScore(){
-    if (result === "Tie"){
-        score.Ties += 1;
-    }else if(result === "Win"){
-        score.Wins += 1
-    }else{score.Losses += 1}
+function reset() {
+  (score.Wins = 0), (score.Losses = 0), (score.Ties = 0);
+
+  document.querySelector(".wins").innerHTML = `${score.Wins}`;
+  document.querySelector(".ties").innerHTML = `${score.Ties}`;
+  document.querySelector(".losses").innerHTML = `${score.Losses}`;
+
+  document.querySelector(".showResult").innerHTML = ``;
 }
 
-function reset(){
-   score.Wins= 0,
-   score.Losses= 0,
-score.Ties = 0;  
+let isAutoPlaying = false;
+let intervalId;
 
-document.querySelector('.wins').innerHTML =`${score.Wins}`; 
-document.querySelector('.ties').innerHTML =`${score.Ties}`;
-document.querySelector('.losses').innerHTML =`${score.Losses}`;
+function autoPlay() {
+  const autoBtn = document.querySelector('.auto-play-button');
 
-document.querySelector('.showResult').innerHTML = ``
-} 
-
-
+  if (!isAutoPlaying) {
+    intervalId = setInterval(function () {
+      const playerChoice = pickComputerMove();
+      play(playerChoice);
+      autoBtn.innerHTML="Stop";
+    }, 1000);
+    isAutoPlaying = true;
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+    autoBtn.innerHTML="Auto Play";
+  }
+}
 
 function play(playerChoice) {
-  let randomNumber = Math.random();
-  let computerChoice = "";
-
-  if (randomNumber <= 0.33) {
-    computerChoice = "Rock";
-  } else if (randomNumber <= 0.66) {
-    computerChoice = "Paper";
-  } else {
-    computerChoice = "Scissors";
-  }
+  const computerChoice = pickComputerMove();
+  let result = "";
 
   if (computerChoice === playerChoice) {
     result = "Tie";
@@ -50,14 +60,27 @@ function play(playerChoice) {
     result = "Lose";
   }
 
-  upgradeScore()
+  upgradeScore(result);
 
-document.querySelector('.wins').innerHTML =`${score.Wins}`; 
-document.querySelector('.ties').innerHTML =`${score.Ties}`;
-document.querySelector('.losses').innerHTML =`${score.Losses}`;
+  document.querySelector(".wins").innerHTML = `${score.Wins}`;
+  document.querySelector(".ties").innerHTML = `${score.Ties}`;
+  document.querySelector(".losses").innerHTML = `${score.Losses}`;
 
-document.querySelector('.showResult').innerHTML = `You picked ${playerChoice}. Computer picked ${computerChoice}. ${result}`
-
+  document.querySelector(
+    ".showResult"
+  ).innerHTML = `You picked ${playerChoice}. Computer picked ${computerChoice}. ${result}`;
 }
 
+function pickComputerMove() {
+  let randomNumber = Math.random();
 
+  if (randomNumber <= 0.33) {
+    computerChoice = "Rock";
+  } else if (randomNumber <= 0.66) {
+    computerChoice = "Paper";
+  } else {
+    computerChoice = "Scissors";
+  }
+
+  return computerChoice;
+}
